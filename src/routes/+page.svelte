f39d85e647cb9600c47071cc08a1a3786c8c0f4b
Peter Repukat <orly.alia5@gmail.com>
2023-04-03 20:33:45 +0200

    resizeable sidebar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
 import MessageDisplay from '../components/MessageDisplay.svelte';
 import SideBar from '../components/SideBar.svelte';
+
+const minPx = 100;
+let sidebarWidth = Number(window.localStorage.getItem('sidebarWidth')) || 0;
+let mouseDown = false;
+
+
+
 </script>
 
-<div class="base-grid">
+<div class="base-grid" style="--sidebarWidth: {sidebarWidth <= minPx ? '25%' : `${sidebarWidth}px`}" on:mouseup={() => mouseDown = false} on:mousemove={(ev) => {
+    if (mouseDown) {
+        sidebarWidth = ev.x;
+        window.localStorage.setItem('sidebarWidth', `${sidebarWidth}`);
+    }
+}}>
     <SideBar />
+    <div class="handle" on:mousedown={() => {
+        console.log('mousedown');
+        mouseDown = true;
+    }}/>
     <MessageDisplay />
 </div>
 
@@ -12,8 +28,14 @@ import SideBar from '../components/SideBar.svelte';
   .base-grid {
     display: grid;
     grid-auto-flow: column;
-    grid-auto-columns: 25% 1fr;
+    grid-auto-columns: var(--sidebarWidth) 5px 1fr;
     position: absolute;
     inset: 0;
   }
+
+  .handle {
+    background-color: #00000075;
+    cursor: ew-resize;
+  }
+
 </style>
